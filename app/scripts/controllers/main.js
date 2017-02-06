47e0f2b384f6073d73bad82b79a31e067ea4392f
Ricardo <ricardo@urbamapp.com>
2017-02-06 15:51:40 -0500

    test de yeomana generator angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testAngularApp
+ */
+angular.module('testAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
